9dd8bf78d7a9493d332196f76d446e49c398bb83
ZShadow01 <57576006+ZShadow01@users.noreply.github.com>
2022-04-26 21:56:17 +0200

    Delete src directory

--- src/Main.java
+++
@@ -1,6 +0,0 @@
-public class Main {
-    public static void main(String[] args) throws Exception {
-        new MainScreen().show();
-        // new Interpreter().readAndExecute("main.bf");
-    }
-}
